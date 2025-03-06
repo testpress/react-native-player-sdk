af2ada8bf0fd8c7d7d67ac6db70202addb3ef2c5
Syed Ibrahim <syed@testpress.in>
2025-03-06 12:19:28 +0530

    feat: initialize player fragment as native modules

--- src/types.ts
+++ src/types.ts
@@ -1,18 +1,5 @@
-export type DRM = {
-  type: 'widevine' | 'playready' | 'fairplay';
-  licenseServer: string;
-  headers?: Record<string, string>;
-  certificateUrl?: string;
-  base64Certificate?: boolean;
-  persistDrmSession?: boolean;
-};
-
-export type Source = {
-  uri: string;
-  drm?: DRM;
-};
-
-export type PlayerProps = {
-  source: Source;
+export type TpStreamsPlayerProps = {
+  videoId: string;
+  accessToken: string;
   style?: import('react-native').ViewStyle;
 };
