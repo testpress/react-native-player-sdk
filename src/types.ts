11ff27504e30491cf66e1552c27367451afb43c2
Gowreesh A M <gowreesh@Gowreeshs-Mac-mini.local>
2025-03-27 19:53:45 +0530

    fix: export player props types

--- src/types.ts
+++ src/types.ts
@@ -3,5 +3,7 @@ export type TpStreamsPlayerProps = {
   accessToken: string;
   enableDownload?: boolean;
   autoPlay?: boolean;
+  startAt?: number;
+  offlineLicenseExpireTime?: number;
   style?: import('react-native').ViewStyle;
 };
