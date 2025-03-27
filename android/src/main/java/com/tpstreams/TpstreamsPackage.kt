e480e1a9fd9d8ca760a56b1cdb7e51d14d833e1c
syed-tp <syed@testpress.in>
2025-03-27 09:22:02 +0530

    chore: add FragmentModule to NativeModules in TpstreamsPackage

--- android/src/main/java/com/tpstreams/TpstreamsPackage.kt
+++ android/src/main/java/com/tpstreams/TpstreamsPackage.kt
@@ -7,7 +7,7 @@ import com.facebook.react.uimanager.ViewManager
 
 class TpstreamsPackage : ReactPackage {
   override fun createNativeModules(reactContext: ReactApplicationContext): List<NativeModule> {
-      return listOf(TpstreamsModule(reactContext))
+      return listOf(TpstreamsModule(reactContext), FragmentModule(reactContext))
   }
 
   override fun createViewManagers(reactContext: ReactApplicationContext): List<ViewManager<*, *>> {
